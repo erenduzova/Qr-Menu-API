6083140e32b200fb314dd459445ee2b1e36d17f7
erenduzova <erenduzova@gmail.com>
2024-03-18 02:52:36 +0300

    Add - StateResponse

---
+++ DTOs/ResponseDTOs/StateResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Qr_Menu_API.DTOs.ResponseDTOs
+{
+    public class StateResponse
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
