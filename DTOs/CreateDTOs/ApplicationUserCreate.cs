15badc0bbc62472cbf7882f917c91be26ceb5881
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-18 12:16:05 +0300

    Add - ApplicationUser Create, Response DTOs

---
+++ DTOs/CreateDTOs/ApplicationUserCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Qr_Menu_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Qr_Menu_API.DTOs.CreateDTOs
+{
+	public class ApplicationUserCreate
+	{
+        [StringLength(100, MinimumLength = 2)]
+        [Column(TypeName = "nvarchar(100)")]
+        public string? UserName { get; set; } = "";
+
+        [StringLength(100, MinimumLength = 2)]
+        [Column(TypeName = "nvarchar(100)")]
+        public string? Name { get; set; } = "";
+
+        [EmailAddress]
+        [StringLength(100, MinimumLength = 5)]
+        [Column(TypeName = "varchar(100)")]
+        public string? Email { get; set; } = "";
+
+        [Phone]
+        [StringLength(30)]
+        [Column(TypeName = "varchar(30)")]
+        public string? PhoneNumber { get; set; } = "";
+
+        public int CompanyId { get; set; }
+
+    }
+}
+
---
+++ DTOs/ResponseDTOs/ApplicationUserResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Qr_Menu_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Qr_Menu_API.DTOs.ResponseDTOs
+{
+	public class ApplicationUserResponse
+	{
+        public string Id { get; set; } = "";
+
+        public string? UserName { get; set; } = "";
+
+        public string? Name { get; set; } = "";
+
+        public string? Email { get; set; } = "";
+
+        public string? PhoneNumber { get; set; } = "";
+
+        public DateTime RegisterDate { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public StateResponse? StateResponse { get; set; }
+    }
+}
+
