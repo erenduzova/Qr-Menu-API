ef0da901d73e88424ec0b2622e6806e107850310
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-18 15:17:10 +0300

    Add - Detailed Response for Restaurant -> Category -> Food

---
+++ DTOs/DetailedResponseDTOs/CategoryDetailedResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Qr_Menu_API.DTOs.ResponseDTOs;
+
+namespace Qr_Menu_API.DTOs.DetailedResponseDTOs
+{
+	public class CategoryDetailedResponse
+	{
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string? Description { get; set; } = "";
+
+        public int RestaurantId { get; set; }
+
+        public StateResponse? StateResponse { get; set; }
+
+        public virtual ICollection<FoodResponse>? FoodsDetailed { get; set; }
+    }
+}
+
---
+++ DTOs/DetailedResponseDTOs/RestaurantDetailedResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Qr_Menu_API.DTOs.DetailedResponseDTOs;
+
+namespace Qr_Menu_API.DTOs.ResponseDTOs
+{
+	public class RestaurantDetailedResponse
+	{
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string? WebAddress { get; set; }
+        public string Phone { get; set; } = "";
+        public string PostalCode { get; set; } = "";
+        public string AddressDetails { get; set; } = "";
+        public DateTime RegisterDate { get; set; }
+        public StateResponse? StateResponse { get; set; }
+        public int? CompanyId { get; set; }
+        public ICollection<CategoryDetailedResponse>? CategoriesDetailed{ get; set; }
+    }
+}
+
