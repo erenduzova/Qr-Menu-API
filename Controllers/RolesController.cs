0f664cb0fc704144ba07efdd251a3af40fde54cf
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-19 11:17:45 +0300

    Add - Roles Controller

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Qr_Menu_API.Controllers
+{
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+    }
+}
+
