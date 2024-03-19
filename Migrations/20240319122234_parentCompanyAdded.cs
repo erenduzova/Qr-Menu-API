3672f5d72699b8417deaf99bc0fcb79f879b719a
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-19 15:23:24 +0300

    Add - Migrations for ParentCompany

---
+++ Migrations/20240319122234_parentCompanyAdded.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Qr_Menu_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class parentCompanyAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCompanyId",
+                table: "Companies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_ParentCompanyId",
+                table: "Companies",
+                column: "ParentCompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Companies_ParentCompanyId",
+                table: "Companies",
+                column: "ParentCompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Companies_ParentCompanyId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_ParentCompanyId",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCompanyId",
+                table: "Companies");
+        }
+    }
+}
