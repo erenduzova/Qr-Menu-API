d2f1b2cd5a40b91f739dcabcc55b6524d4ffa013
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-21 10:47:19 +0300

    Add - Create and Get Methods for RestaurantUsers to Controller and Service

--- Controllers/RestaurantUsersController.cs
+++ Controllers/RestaurantUsersController.cs
@@ -6,7 +6,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Qr_Menu_API.Data;
+using Qr_Menu_API.DTOs.CreateDTOs;
+using Qr_Menu_API.DTOs.ResponseDTOs;
 using Qr_Menu_API.Models;
+using Qr_Menu_API.Services;
 
 namespace Qr_Menu_API.Controllers
 {
@@ -15,122 +18,111 @@ namespace Qr_Menu_API.Controllers
     public class RestaurantUsersController : ControllerBase
     {
         private readonly ApplicationContext _context;
+        private readonly RestaurantUsersService _restaurantUsersService;
 
-        public RestaurantUsersController(ApplicationContext context)
+        public RestaurantUsersController(ApplicationContext context, RestaurantUsersService restaurantUsersService)
         {
             _context = context;
+            _restaurantUsersService = restaurantUsersService;
         }
 
         // GET: api/RestaurantUsers
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<RestaurantUser>>> GetRestaurantUsers()
-        {
-          if (_context.RestaurantUsers == null)
-          {
-              return NotFound();
-          }
-            return await _context.RestaurantUsers.ToListAsync();
-        }
-
-        // GET: api/RestaurantUsers/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<RestaurantUser>> GetRestaurantUser(string id)
-        {
-          if (_context.RestaurantUsers == null)
-          {
-              return NotFound();
-          }
-            var restaurantUser = await _context.RestaurantUsers.FindAsync(id);
-
-            if (restaurantUser == null)
-            {
-                return NotFound();
-            }
-
-            return restaurantUser;
-        }
-
-        // PUT: api/RestaurantUsers/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutRestaurantUser(string id, RestaurantUser restaurantUser)
-        {
-            if (id != restaurantUser.UserId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(restaurantUser).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RestaurantUserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/RestaurantUsers
-        [HttpPost]
-        public async Task<ActionResult<RestaurantUser>> PostRestaurantUser(RestaurantUser restaurantUser)
-        {
-          if (_context.RestaurantUsers == null)
-          {
-              return Problem("Entity set 'ApplicationContext.RestaurantUsers'  is null.");
-          }
-            _context.RestaurantUsers.Add(restaurantUser);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (RestaurantUserExists(restaurantUser.UserId))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetRestaurantUser", new { id = restaurantUser.UserId }, restaurantUser);
-        }
-
-        // DELETE: api/RestaurantUsers/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteRestaurantUser(string id)
+        public ActionResult<List<RestaurantUserResponse>> GetRestaurantUsers()
         {
             if (_context.RestaurantUsers == null)
             {
                 return NotFound();
             }
-            var restaurantUser = await _context.RestaurantUsers.FindAsync(id);
+
+            return _restaurantUsersService.GetRestaurantUserResponses();
+        }
+
+        // GET: api/RestaurantUsers/5
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantUserResponse> GetRestaurantUser(string id)
+        {
+            if (_context.RestaurantUsers == null)
+            {
+                return NotFound();
+            }
+            
+            var restaurantUser = _context.RestaurantUsers.Find(id);
+
             if (restaurantUser == null)
             {
                 return NotFound();
             }
 
-            _context.RestaurantUsers.Remove(restaurantUser);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return _restaurantUsersService.GetRestaurantUserResponse(restaurantUser);
         }
 
-        private bool RestaurantUserExists(string id)
+        //    // PUT: api/RestaurantUsers/5
+        //    [HttpPut("{id}")]
+        //    public async Task<IActionResult> PutRestaurantUser(string id, RestaurantUser restaurantUser)
+        //    {
+        //        if (id != restaurantUser.UserId)
+        //        {
+        //            return BadRequest();
+        //        }
+
+        //        _context.Entry(restaurantUser).State = EntityState.Modified;
+
+        //        try
+        //        {
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!RestaurantUserExists(id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+
+        //        return NoContent();
+        //    }
+
+        // POST: api/RestaurantUsers
+        [HttpPost]
+        public ActionResult<RestaurantUserResponse> PostRestaurantUser(RestaurantUserCreate restaurantUserCreate)
         {
-            return (_context.RestaurantUsers?.Any(e => e.UserId == id)).GetValueOrDefault();
+            if (_context.RestaurantUsers == null)
+            {
+                return Problem("Entity set 'ApplicationContext.RestaurantUsers'  is null.");
+            }
+            
+            return _restaurantUsersService.AddRestaurantUser(restaurantUserCreate);
+
         }
+
+        //    // DELETE: api/RestaurantUsers/5
+        //    [HttpDelete("{id}")]
+        //    public async Task<IActionResult> DeleteRestaurantUser(string id)
+        //    {
+        //        if (_context.RestaurantUsers == null)
+        //        {
+        //            return NotFound();
+        //        }
+        //        var restaurantUser = await _context.RestaurantUsers.FindAsync(id);
+        //        if (restaurantUser == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        _context.RestaurantUsers.Remove(restaurantUser);
+        //        await _context.SaveChangesAsync();
+
+        //        return NoContent();
+        //    }
+
+        //    private bool RestaurantUserExists(string id)
+        //    {
+        //        return (_context.RestaurantUsers?.Any(e => e.UserId == id)).GetValueOrDefault();
+        //    }
     }
 }
--- Services/RestaurantUsersService.cs
+++ Services/RestaurantUsersService.cs
@@ -1,9 +1,61 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Qr_Menu_API.Data;
+using Qr_Menu_API.DTOs.CreateDTOs;
+using Qr_Menu_API.DTOs.ResponseDTOs;
+using Qr_Menu_API.Models;
+
 namespace Qr_Menu_API.Services
 {
 	public class RestaurantUsersService
 	{
-		
-	}
+        private readonly ApplicationContext _context;
+        private readonly UsersService _usersService;
+        private readonly RestaurantsService _restaurantsService;
+
+        public RestaurantUsersService(ApplicationContext context, UsersService usersService, RestaurantsService restaurantsService)
+        {
+            _context = context;
+            _usersService = usersService;
+            _restaurantsService = restaurantsService;
+        }
+
+        public RestaurantUserResponse AddRestaurantUser(RestaurantUserCreate restaurantUserCreate)
+        {
+            RestaurantUser restaurantUser = new()
+            {
+                RestaurantId = restaurantUserCreate.RestaurantId,
+                UserId = restaurantUserCreate.UserId
+            };
+            _context.RestaurantUsers!.Add(restaurantUser);
+            RestaurantUserResponse restaurantUserResponse = new()
+            {
+                RestaurantId = restaurantUser.RestaurantId,
+                UserId = restaurantUser.UserId
+            };
+            return restaurantUserResponse;
+        }
+
+        public List<RestaurantUserResponse> GetRestaurantUserResponses()
+        {
+            var restaurantUsers = _context.RestaurantUsers!.ToList();
+            List<RestaurantUserResponse> restaurantUserResponses = new List<RestaurantUserResponse>();
+            foreach (RestaurantUser restaurantUser in restaurantUsers)
+            {
+                restaurantUserResponses.Add(GetRestaurantUserResponse(restaurantUser));
+            }
+            return restaurantUserResponses;
+        }
+
+        public RestaurantUserResponse GetRestaurantUserResponse(RestaurantUser restaurantUser)
+        {
+            RestaurantUserResponse restaurantUserResponse = new()
+            {
+                RestaurantId = restaurantUser.RestaurantId,
+                UserId = restaurantUser.UserId
+            };
+            return restaurantUserResponse;
+        }
+    }
 }
 
