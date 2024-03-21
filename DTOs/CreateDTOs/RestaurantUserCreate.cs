d260e28994f03743f6de872ba5c8cb0519c207c2
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-21 10:12:34 +0300

    Add - RestaurantUser response, create

---
+++ DTOs/CreateDTOs/RestaurantUserCreate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Qr_Menu_API.DTOs.CreateDTOs
+{
+	public class RestaurantUserCreate
+	{
+        public int RestaurantId { get; set; }
+        public string UserId { get; set; } = "";
+    }
+}
+
---
+++ DTOs/ResponseDTOs/RestaurantUserResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Qr_Menu_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Qr_Menu_API.DTOs.ResponseDTOs
+{
+	public class RestaurantUserResponse
+	{
+        public int RestaurantId { get; set; }
+        public string UserId { get; set; } = "";
+    }
+}
+
